c49143fbd0fd2dc9d8ec78eef03d26fa5e4e5c95
ncg201064@gmail.com Glascock <ncg201064@gmail.com>
2018-05-02 06:27:48 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 var katzDeli = [];
 
 function takeANumber(katzDeliLine, name){
-  
+  if (katzDeli.length === 0) {
+    katzDeli[0] = Object.assign({}, katzDeli, {[katzDeliLine]: name});
+  } else {
+    katzDeli[katzDeli.length + 1] = Object.assign({}, katzDeli, {[katzDeliLine]: name});
+  }
+  return katzDeli;
 }
